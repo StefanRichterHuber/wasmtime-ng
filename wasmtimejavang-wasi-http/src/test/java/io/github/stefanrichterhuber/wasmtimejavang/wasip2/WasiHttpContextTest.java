818ecf54a997744b8fecbf85ea9248001918254c
Stefan Richter-Huber <stefan@richter-huber.de>
2026-07-20 09:33:25 +0200

    Further cleanup of tests

--- wasmtimejavang-wasi-http/src/test/java/io/github/stefanrichterhuber/wasmtimejavang/wasip2/WasiHttpContextTest.java
+++ wasmtimejavang-wasi-http/src/test/java/io/github/stefanrichterhuber/wasmtimejavang/wasip2/WasiHttpContextTest.java
@@ -1,6 +1,5 @@
 package io.github.stefanrichterhuber.wasmtimejavang.wasip2;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
